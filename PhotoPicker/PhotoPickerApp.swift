aa0ad82551c894052f548dc51e4e7aec7581266f
Sabri Demir <safvcdsa161@gmail.com>
2025-09-27 22:30:00 +0300

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Sabri Demir on 27.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
